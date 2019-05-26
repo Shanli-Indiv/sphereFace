e668a5398003c68df8de583e4d4d466993799178
Shanli-Indiv <yushanlinba@gmail.com>
2019-05-25 22:30:31 -0700

    Now can convert to curve and added batch norm

--- showImage.py
+++ showImage.py
@@ -1,14 +1,33 @@
 import numpy as np
 from PIL import Image
+#from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt 
 
-img_array = np.load('loss.npy')
+
+
+img_array = np.load('loss.npy') 
 print(img_array)
 print(img_array.shape)
 
-im = Image.fromarray(img_array, 'RGB')
+#img_array = img_array.reshape(200,140)
+print(img_array.shape)
+
+# im = Image.fromarray(img_array, 'L')
 # this might fail if `img_array` contains a data type that is not supported by PIL,
 # in which case you could try casting it to a different dtype e.g.:
 # im = Image.fromarray(img_array.astype(np.uint8))
+# im.show()
+# plt.plot(img_array, np.zeros_like(img_array) + 0, 'x')
+plt.plot(img_array)
 
 
-im.show()
+plt.show()
+
+#plt.imshow(img_array, cmap='gray')
+
+
+# digits = img_array
+# print(digits.data.shape)
+# plt.gray() 
+# plt.matshow(digits.images[0]) 
+# plt.show() 
